9cce1bce820ff200580f4a3661df225406a241c4
bharathchandrareddy <bharathreddy478@gmail.com>
2025-07-04 10:52:12 -0500

    added efficient method for 3rd question in arrays

--- 3rd_question.py
+++ 3rd_question.py
@@ -24,3 +24,36 @@ def replace_str(string:str, len_of_str:int) -> bool:
 print(replace_str('mr John Smith               ',13))
 
 #efficient algorithm, use backward inplace replacement
+def urlify(string:str,true_length:int)->str:
+    '''
+    time complexity: O(n)
+    space complexity : O(1)
+    '''
+    string = list(string)
+    # get the count of spaces in string
+    space_count = 0
+    for i in range(true_length):
+        if string[i] == ' ':
+            space_count+=1
+    #print('space count ',space_count)
+    index = true_length + 2*space_count
+    #print('index = ',index)
+
+    for char in range(true_length-1,-1,-1):
+        if string[char] == ' ':
+            string[index-1] = '0'
+            #print(string)
+            string[index-2] = '2'
+            #print(string)
+            string[index-3] = '%'
+            #print(string)
+            index-=3
+        else:
+            string[index-1] = string[char]
+            #print(string)
+            index-=1
+    #print(str(string))
+    return string
+
+string = urlify('mr John Smith               ',13)
+print("".join(string))
