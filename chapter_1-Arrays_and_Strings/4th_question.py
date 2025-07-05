4b733b057b6f899ed36da4aba38daedf2d05be0e
bharathchandrareddy <bharathreddy478@gmail.com>
2025-07-05 13:37:28 -0500

    rearraged the questions and added 4th question

---
+++ chapter_1-Arrays_and_Strings/4th_question.py
@@ -0,0 +1,30 @@
+'''
+question: given a string, write a function to check if it is a permutation of a palindrome.
+example:
+input: Tact Coa
+output: True   #(the palindrome of input is 'atco cta')
+Note: Non-letter characters are not required, CASE insensitive
+'''
+# by using hash table/dictionary
+def permutation_palindrome(string:str) -> bool:
+    '''
+    time complexity=O(n)
+    space complexity=
+    '''
+    string = string.lower().replace(' ','')
+    odd_count = 0
+    char_count = {}
+    for char in string:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+    for item in char_count:
+        if char_count[item] %2 == 1:
+            odd_count+=1
+        if odd_count>1:
+            return False
+    return True
+
+print(permutation_palindrome('tact coa'))
+
