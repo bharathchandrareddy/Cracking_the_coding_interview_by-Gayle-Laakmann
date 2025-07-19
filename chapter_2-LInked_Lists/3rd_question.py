b340ec68ad38e9bfbf8675c59f331d65d8660b73
bharathchandrareddy <bharathreddy478@gmail.com>
2025-07-18 22:36:07 -0500

    just added the question to not miss the streak

---
+++ chapter_2-LInked_Lists/3rd_question.py
@@ -0,0 +1,7 @@
+'''
+Delete the middle node: Implement an algorithm to delete a node in the middle(i.e any node but first and last node) of a single linked list
+given only access to that node.
+example:
+input: remove c from the linked list a->b->c->d->e->f
+output: nothing is returned, but the new linked list looks like a->b->d->e->f
+'''
